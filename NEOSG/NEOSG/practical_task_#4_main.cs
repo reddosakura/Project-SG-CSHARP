1129e10884c8d96bed7040b3591099661866d542
reddosakura <alexander05511@gmail.com>
2022-10-20 13:59:45 +0300

    [commit] -

---
+++ NEOSG/NEOSG/practical_task_#4_main.cs
@@ -0,0 +1,22 @@
+﻿using NEONOTE;
+namespace NEOSG;
+
+
+public class practical_task__4_main
+{
+    public static DateTime date_now = DateTime.Now;
+    private static int day_step = 0;
+
+    static void GetNoteInfo()
+    {
+        
+    }
+    static void Menu()
+    {
+        Console.WriteLine($"\n----------------\n {date_now.AddDays(day_step)} \n----------------\n");
+    }
+    static void Main()
+    {
+        // Note note = new Note();
+    }
+}
